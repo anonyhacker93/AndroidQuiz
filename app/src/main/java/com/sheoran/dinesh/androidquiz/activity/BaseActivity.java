44859c891c2217d29fcf88164f7622e6d6bd5338
Pratibha <ssaini@meritech.co.in>
2018-12-06 11:56:39 +0530

    Added Navigation drawer.

--- app/src/main/java/com/sheoran/dinesh/androidquiz/activity/BaseActivity.java
+++ app/src/main/java/com/sheoran/dinesh/androidquiz/activity/BaseActivity.java
@@ -3,9 +3,27 @@ package com.sheoran.dinesh.androidquiz.activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.sheoran.dinesh.androidquiz.R;
+
 //Dinesh Sheoran's
 public class BaseActivity  extends AppCompatActivity {
+    protected Toolbar toolbar;
+
+    public void setToolbar(int drawable) {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        if(ab!=null) {
+            ab.setHomeAsUpIndicator(drawable);
+            ab.setDisplayShowHomeEnabled(true);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+    }
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/sheoran/dinesh/androidquiz/activity/MainActivity.java
+++ app/src/main/java/com/sheoran/dinesh/androidquiz/activity/MainActivity.java
@@ -2,6 +2,9 @@ package com.sheoran.dinesh.androidquiz.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 
 import com.sheoran.dinesh.androidquiz.R;
 import com.sheoran.dinesh.androidquiz.activity.BaseActivity;
@@ -9,11 +12,15 @@ import com.sheoran.dinesh.androidquiz.fragment.CategoriesFragment;
 import com.sheoran.dinesh.androidquiz.fragment.HomeFragment;
 
 public class MainActivity extends BaseActivity {
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        setToolbar(R.drawable.ic_menu);
         loadHomeFragment();
     }
 
@@ -24,9 +31,20 @@ public class MainActivity extends BaseActivity {
         transaction.commit();
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
     }
+
+
 }
